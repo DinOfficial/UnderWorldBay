cb45e2e7a8eb4184d580395d472e7f920e43c10a
DinOfficial <shdinofficial19@gmail.com>
2026-02-04 15:38:13 +0300

    set localization using provider

---
+++ lib/app/provider/language_provider.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+
+class LanguageProvider extends ChangeNotifier {
+  Locale _currentLocal = Locale('en');
+  final List<Locale> _supportedLocales = [Locale('en'), Locale('bn')];
+
+  List<Locale> get supportedLocales => _supportedLocales;
+
+  Locale get currentLocal => _currentLocal;
+
+  void changeLocale(Locale locale) {
+    _currentLocal = locale;
+    notifyListeners();
+  }
+}
--- lib/app/underworld_bay_app.dart
+++ lib/app/underworld_bay_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 import 'package:underworld_bay/app/app_theme.dart';
+import 'package:underworld_bay/app/provider/language_provider.dart';
 import 'package:underworld_bay/l10n/app_localizations.dart';
 import '../features/auth/presentation/screens/splash_screen.dart';
 
@@ -9,24 +11,26 @@ class UnderWorldBayApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      locale:Locale('bn'),
-      supportedLocales: [
-        Locale('en'),
-        Locale('bn'),
-      ],
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      themeMode: ThemeMode.system,
-      // initialRoute: '/',
-      // onGenerateRoute: AppRoutes.onGenerateRoute,
-      home:SplashScreen(),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => LanguageProvider())],
+      child: Consumer<LanguageProvider>(
+        builder: (context, languageProvider, child) {
+          return MaterialApp(
+            localizationsDelegates: [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            locale: languageProvider.currentLocal,
+            supportedLocales: [Locale('en'), Locale('bn')],
+            theme: AppTheme.light,
+            darkTheme: AppTheme.dark,
+            themeMode: ThemeMode.system,
+            home: SplashScreen(),
+          );
+        },
+      ),
     );
   }
 }
