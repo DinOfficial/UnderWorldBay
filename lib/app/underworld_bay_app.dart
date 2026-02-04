42248d8c8a42888e8b9d7c24ddc3265d9d39d727
DinOfficial <shdiinofficial19@gmail.com>
2026-02-04 12:59:16 +0300

    localization

--- lib/app/underworld_bay_app.dart
+++ lib/app/underworld_bay_app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:underworld_bay/app/app_theme.dart';
-
+import 'package:underworld_bay/l10n/app_localizations.dart';
 import '../features/auth/presentation/screens/splash_screen.dart';
-import 'app_routes.dart';
 
 class UnderWorldBayApp extends StatelessWidget {
   const UnderWorldBayApp({super.key});
@@ -10,6 +10,17 @@ class UnderWorldBayApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale:Locale('bn'),
+      supportedLocales: [
+        Locale('en'),
+        Locale('bn'),
+      ],
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       themeMode: ThemeMode.system,
