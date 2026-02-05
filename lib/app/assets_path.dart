4ed9f353862b86301dd3071fee0fef0a46158027
DinOfficial <shdinofficial19@gmail.com>
2026-02-05 19:58:16 +0300

    setup splash screen

---
+++ lib/app/assets_path.dart
@@ -0,0 +1,5 @@
+class AssetsPath {
+  static const String _imagePath = 'assets/images';
+  static const String svgLogo = '$_imagePath/logo.svg';
+  static const String navSvgLogo = '$_imagePath/logo_nav.svg';
+}
--- lib/features/auth/presentation/screens/splash_screen.dart
+++ lib/features/auth/presentation/screens/splash_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:underworld_bay/app/extentions/localization_extention.dart';
-import 'package:underworld_bay/l10n/app_localizations.dart';
-import '../../../../app/providers/language_provider.dart';
-import '../../../../app/providers/theme_mode_provider.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:underworld_bay/app/assets_path.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -15,38 +12,14 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    final languageProvider = Provider.of<LanguageProvider>(context);
-    final themeProvider = Provider.of<ThemeModeProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(context.l10n.hello),
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: 8),
-            child: DropdownMenu(
-              initialSelection: languageProvider.currentLocal,
-              dropdownMenuEntries: languageProvider.supportedLocales
-                  .map((e) => DropdownMenuEntry(value: e, label: e.languageCode))
-                  .toList(),
-              onSelected: (value) {
-                languageProvider.changeLocale(value as Locale);
-              },
-            ),
-          ),
-
-          Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: DropdownMenu(
-              initialSelection: themeProvider.themeMode,
-              dropdownMenuEntries: themeProvider.themeModes
-                  .map((e) => DropdownMenuEntry(value: e, label: e.name))
-                  .toList(),
-              onSelected: (value) {
-                themeProvider.themeModeChange(value as ThemeMode);
-              },
-            ),
-          ),
-        ],
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Expanded(child: SvgPicture.asset(AssetsPath.svgLogo))
+          ],
+        ),
       ),
     );
   }
