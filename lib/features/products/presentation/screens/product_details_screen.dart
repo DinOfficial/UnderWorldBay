78b33366edd4550e9f9431c9cb25ead0411ced24
DinOfficial <shdinofficial19@gmail.com>
2026-02-26 01:47:26 +0300

    [WIP] product details screen ui

--- lib/features/products/presentation/screens/product_details_screen.dart
+++ lib/features/products/presentation/screens/product_details_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:underworld_bay/app/app_colors.dart';
-import 'package:underworld_bay/app/constrants.dart';
-import 'package:underworld_bay/app/extentions/utils_extention.dart';
+
+import '../widgets/price_and_add_to_cart_section.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
   const ProductDetailsScreen({super.key});
@@ -21,33 +19,7 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
       body: Column(
         children: [
           Expanded(child: SizedBox()),
-          Container(
-            padding: .all(16),
-            decoration: BoxDecoration(
-              color: AppColors.themeColor.withAlpha(30),
-              borderRadius: .only(topRight: Radius.circular(16), topLeft: Radius.circular(16)),
-            ),
-            child: Row(
-              mainAxisAlignment: .spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    Text('Price', style: context.textTheme.bodyLarge,),
-                    Text('${Constants.takaSign} 1000', style: context.textTheme.titleLarge?.copyWith(color: AppColors.themeColor),),
-                  ],
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.themeColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: .circular(8)
-                    )
-                  ),
-                    onPressed: () {},
-                    child: Text('Add to cart', style: TextStyle(color: Colors.white),))
-              ],
-            ),
-          ),
+          PriceAndAddToCartSection(price: 1000, onTapAddToCart: () {  },),
         ],
       ),
     );
