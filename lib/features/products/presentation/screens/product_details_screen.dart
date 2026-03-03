65a1779cd86cf6ed755b41e3313060392a372851
DinOfficial <shdinofficial19@gmail.com>
2026-03-04 01:35:26 +0300

    [WIF]product details screen ui

--- lib/features/products/presentation/screens/product_details_screen.dart
+++ lib/features/products/presentation/screens/product_details_screen.dart
@@ -1,12 +1,10 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../app/app_colors.dart';
 import '../widgets/price_and_add_to_cart_section.dart';
+import '../widgets/product_details_carousel.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
   const ProductDetailsScreen({super.key});
-
   static const String name = '/product-details';
 
   @override
@@ -14,7 +12,6 @@ class ProductDetailsScreen extends StatefulWidget {
 }
 
 class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
-  final ValueNotifier<int> _currentIndex = ValueNotifier<int>(0);
 
   @override
   Widget build(BuildContext context) {
@@ -25,61 +22,7 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
           Expanded(
             child: Column(
               children: [
-                Stack(
-                  children: [
-                    CarouselSlider(
-                      options: CarouselOptions(
-                        height: 180.0,
-                        viewportFraction: 1,
-                        autoPlay: true,
-                        onPageChanged: (index, reason) {
-                          _currentIndex.value = index;
-                        },
-                      ),
-                      items: [1, 2, 3, 4, 5].map((i) {
-                        return Builder(
-                          builder: (BuildContext context) {
-                            return Container(
-                              width: MediaQuery.of(context).size.width,
-                              margin: EdgeInsets.symmetric(horizontal: 5.0),
-                              decoration: BoxDecoration(
-                                color: Colors.amber,
-                                borderRadius: .circular(8),
-                              ),
-                              child: Center(
-                                child: Text('text $i', style: TextStyle(fontSize: 16.0)),
-                              ),
-                            );
-                          },
-                        );
-                      }).toList(),
-                    ),
-                    Positioned(
-                      bottom: 5,
-                      left: MediaQuery.of(context).size.width * .425,
-                      child: ValueListenableBuilder(
-                        valueListenable: _currentIndex,
-                        builder: (context, value, child) {
-                          return Row(
-                            mainAxisAlignment: .center,
-                            children: [
-                              for (var i = 0; i < 5; i++)
-                                Container(
-                                  height: 12,
-                                  width: 12,
-                                  margin: .only(right: 4),
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: value == i ? AppColors.themeColor : Colors.white,
-                                  ),
-                                ),
-                            ],
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
+                ProductDetailsCarousel(),
               ],
             ),
           ),
---
+++ lib/features/products/presentation/widgets/product_details_carousel.dart
@@ -0,0 +1,77 @@
+
+
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../app/app_colors.dart';
+
+class ProductDetailsCarousel extends StatefulWidget {
+  const ProductDetailsCarousel({
+    super.key,
+  });
+
+
+  @override
+  State<ProductDetailsCarousel> createState() => _ProductDetailsCarouselState();
+}
+
+class _ProductDetailsCarouselState extends State<ProductDetailsCarousel> {
+  final ValueNotifier<int> _currentIndex = ValueNotifier<int>(0);
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            height: 180.0,
+            viewportFraction: 1,
+            autoPlay: true,
+            onPageChanged: (index, reason) {
+              _currentIndex.value = index;
+            },
+          ),
+          items: [1, 2, 3, 4, 5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(
+                    color: AppColors.themeColor.withAlpha(30),
+                  ),
+                  child: Center(
+                    child: Text('text $i', style: TextStyle(fontSize: 16.0)),
+                  ),
+                );
+              },
+            );
+          }).toList(),
+        ),
+        Positioned(
+          bottom: 5,
+          left: MediaQuery.of(context).size.width * .425,
+          child: ValueListenableBuilder(
+            valueListenable: _currentIndex,
+            builder: (context, value, child) {
+              return Row(
+                mainAxisAlignment: .center,
+                children: [
+                  for (var i = 0; i < 5; i++)
+                    Container(
+                      height: 12,
+                      width: 12,
+                      margin: .only(right: 4),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: value == i ? AppColors.themeColor : Colors.white,
+                      ),
+                    ),
+                ],
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
