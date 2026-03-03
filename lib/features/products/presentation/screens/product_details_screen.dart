7f805b14eaa0c50b3cba7c73e0c62b98f1663b4f
DinOfficial <shdinofficial19@gmail.com>
2026-03-04 01:19:30 +0300

    product carousel ui compete

--- lib/features/products/presentation/screens/product_details_screen.dart
+++ lib/features/products/presentation/screens/product_details_screen.dart
@@ -1,5 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../app/app_colors.dart';
 import '../widgets/price_and_add_to_cart_section.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
@@ -12,14 +14,76 @@ class ProductDetailsScreen extends StatefulWidget {
 }
 
 class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  final ValueNotifier<int> _currentIndex = ValueNotifier<int>(0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.white, title: Text('Product Details')),
       body: Column(
         children: [
-          Expanded(child: SizedBox()),
-          PriceAndAddToCartSection(price: 1000, onTapAddToCart: () {  },),
+          Expanded(
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    CarouselSlider(
+                      options: CarouselOptions(
+                        height: 180.0,
+                        viewportFraction: 1,
+                        autoPlay: true,
+                        onPageChanged: (index, reason) {
+                          _currentIndex.value = index;
+                        },
+                      ),
+                      items: [1, 2, 3, 4, 5].map((i) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin: EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: BoxDecoration(
+                                color: Colors.amber,
+                                borderRadius: .circular(8),
+                              ),
+                              child: Center(
+                                child: Text('text $i', style: TextStyle(fontSize: 16.0)),
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                    ),
+                    Positioned(
+                      bottom: 5,
+                      left: MediaQuery.of(context).size.width * .425,
+                      child: ValueListenableBuilder(
+                        valueListenable: _currentIndex,
+                        builder: (context, value, child) {
+                          return Row(
+                            mainAxisAlignment: .center,
+                            children: [
+                              for (var i = 0; i < 5; i++)
+                                Container(
+                                  height: 12,
+                                  width: 12,
+                                  margin: .only(right: 4),
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: value == i ? AppColors.themeColor : Colors.white,
+                                  ),
+                                ),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          PriceAndAddToCartSection(price: 1000, onTapAddToCart: () {}),
         ],
       ),
     );
